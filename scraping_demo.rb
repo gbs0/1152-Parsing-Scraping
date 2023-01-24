57823c14dadb0ec0d960cc48553e4235e9d463c5
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-01-24 12:04:12 -0300

    automatted commit

--- scraping_demo.rb
+++ scraping_demo.rb
@@ -10,15 +10,13 @@ url = "https://www.bbcgoodfood.com/search?q=#{ingredient}"
 html_file = URI.open(url).read
 document = Nokogiri::HTML.parse(html_file)
 
+# Buscamos pela classe, qual o elemento HTML que contém cada receita
 recipes = document.search(".standard-card-new--skinny")
 
+# Iteramos sobre cada elemento encontrado, mostrando seu nome e link de acesso
 recipes.each_with_index do |recipe, index|
   recipe_name = recipe.search(".standard-card-new__article-title").text.strip
   recipe_link = recipe.search(".standard-card-new__article-title").attribute('href')
   puts "#{index + 1} | #{recipe_name} - See more: https://www.bbcgoodfood.com#{recipe_link}"
-  
 end
 
-# 1 - Bolo de chocolate - See more: https://www.bbcgoodfood.com/recipes/best-ever-chocolate-brownies-recipe
-# 2 - Chocolate cake with slice cut on cake stand
-# 3 - Ultimate chocolate cake 
